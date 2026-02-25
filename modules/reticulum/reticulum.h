4daad9e92de1652f25693159d6e7b782765b13eb
guardmansduck <100035465@mvla.net>
2026-02-25 13:55:41 -0800

    Create reticulum.h

---
+++ modules/reticulum/reticulum.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "../../core/transport.h"
+#include "../../core/packet.h"
+#include <Arduino.h>
+
+class ReticulumModule : public Transport {
+private:
+    void (*rxCallback)(const OpenRF_Packet&) = nullptr;
+
+public:
+    ReticulumModule() {}
+
+    const char* name() override { return "reticulum"; }
+    void begin() override;
+    bool send(const OpenRF_Packet &pkt) override;
+    void loop() override;
+    void setReceiveCallback(void (*cb)(const OpenRF_Packet&)) override;
+};
