8d84b23b0d5ca3f0625b6227d7d704b6506ea57d
guardmansduck <100035465@mvla.net>
2026-02-25 13:26:06 -0800

    Remove comments from OAuthManager methods
    Removed comments from OAuthManager class methods.

--- firmware/oauth/oauth.h
+++ firmware/oauth/oauth.h
@@ -14,24 +14,20 @@ class OAuthManager {
 public:
     OAuthManager() {}
 
-    // Get URL for user authorization
     String getAuthUrl(const String &clientId,
                       const String &redirectUri,
                       const String &scope,
                       const String &state);
 
-    // Exchange authorization code for access token
     bool exchangeCodeForToken(const String &clientId,
                               const String &clientSecret,
                               const String &redirectUri,
                               const String &code,
                               OAuthToken &token);
 
-    // Refresh access token using refresh token
     bool refreshAccessToken(const String &clientId,
                             const String &clientSecret,
                             OAuthToken &token);
 
-    // Check if token is valid (not expired)
     bool isTokenValid(const OAuthToken &token);
 };
