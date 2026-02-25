1cd1b61bec6fe4b5677faf4ee4647516acde3e44
guardmansduck <100035465@mvla.net>
2026-02-25 13:41:45 -0800

    Condense error handling in OAuth token requests

--- firmware/oauth/oauth.cpp
+++ firmware/oauth/oauth.cpp
@@ -21,7 +21,6 @@ bool OAuthManager::exchangeCodeForToken(const String &clientId,
     HTTPClient http;
     http.begin("https://discord.com/api/oauth2/token");
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
     String body = "client_id=" + clientId +
                   "&client_secret=" + clientSecret +
                   "&grant_type=authorization_code" +
@@ -29,21 +28,15 @@ bool OAuthManager::exchangeCodeForToken(const String &clientId,
                   "&redirect_uri=" + redirectUri;
 
     int httpCode = http.POST(body);
-    if (httpCode != 200) {
-        http.end();
-        return false;
-    }
+    if (httpCode != 200) { http.end(); return false; }
 
     String resp = http.getString();
     http.end();
-
     DynamicJsonDocument doc(1024);
     deserializeJson(doc, resp);
-
     token.accessToken = doc["access_token"].as<String>();
     token.refreshToken = doc["refresh_token"].as<String>();
     token.expiresAt = millis() / 1000 + doc["expires_in"].as<uint32_t>();
-
     return true;
 }
 
@@ -55,28 +48,21 @@ bool OAuthManager::refreshAccessToken(const String &clientId,
     HTTPClient http;
     http.begin("https://discord.com/api/oauth2/token");
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
     String body = "client_id=" + clientId +
                   "&client_secret=" + clientSecret +
                   "&grant_type=refresh_token" +
                   "&refresh_token=" + token.refreshToken;
 
     int httpCode = http.POST(body);
-    if (httpCode != 200) {
-        http.end();
-        return false;
-    }
+    if (httpCode != 200) { http.end(); return false; }
 
     String resp = http.getString();
     http.end();
-
     DynamicJsonDocument doc(1024);
     deserializeJson(doc, resp);
-
     token.accessToken = doc["access_token"].as<String>();
     token.refreshToken = doc["refresh_token"].as<String>();
     token.expiresAt = millis() / 1000 + doc["expires_in"].as<uint32_t>();
-
     return true;
 }
 
