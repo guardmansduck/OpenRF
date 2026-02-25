4108d95eb4f3052e0416e556dc3a52cbc01f6bc4
guardmansduck <100035465@mvla.net>
2026-02-25 13:28:02 -0800

    Refactor LoRaModule methods and improve formatting

--- modules/lora/lora.cpp
+++ modules/lora/lora.cpp
@@ -1,30 +1,26 @@
 #include "lora.h"
 
-const char* LoRaModule::name() {
-    return "lora";
-}
+const char* LoRaModule::name() { return "lora"; }
 
 void LoRaModule::begin() {
     BoardPins pins = detectBoardPins();
-    Serial.printf("LoRa pins auto-detected: NSS=%d, RESET=%d, DIO0=%d\n", pins.ss, pins.reset, pins.dio0);
+    Serial.printf("LoRa pins: NSS=%d, RESET=%d, DIO0=%d\n", pins.ss, pins.reset, pins.dio0);
 
     SPI.begin();
     LoRa.setPins(pins.ss, pins.reset, pins.dio0);
 
     if (!LoRa.begin(915E6)) {
         Serial.println("LoRa init failed!");
-        while (1);
+        while(1);
     }
 
     LoRa.onReceive([](int packetSize){
         if (packetSize == sizeof(OpenRF_Packet)) {
             OpenRF_Packet pkt;
-            for (int i = 0; i < packetSize; i++) {
+            for (int i = 0; i < packetSize; i++)
                 ((uint8_t*)&pkt)[i] = LoRa.read();
-            }
-            if (LoRaModule::rxCallback) {
+            if (LoRaModule::rxCallback)
                 LoRaModule::rxCallback(pkt);
-            }
         }
     });
 
@@ -37,9 +33,7 @@ bool LoRaModule::send(const OpenRF_Packet &pkt) {
     return LoRa.endPacket() == 1;
 }
 
-void LoRaModule::loop() {
-    // nothing; receive handled asynchronously
-}
+void LoRaModule::loop() {}
 
 void LoRaModule::setReceiveCallback(void (*cb)(const OpenRF_Packet&)) {
     rxCallback = cb;
