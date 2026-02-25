bf59504b7f86bf30e2126f301f7ce5e6d33ab447
guardmansduck <100035465@mvla.net>
2026-02-25 14:03:06 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 #include "core/router.h"
-#include "core/packet.h"
-
 #include "modules/lora/lora.h"
 #include "modules/meshtastic/meshtastic.h"
 #include "modules/reticulum/reticulum.h"
@@ -10,7 +8,6 @@
 #include "modules/slack/slack.h"
 #include "modules/gmail/gmail.h"
 #include "modules/steam/steam.h"
-
 #include "oauth/oauth.h"
 #include "config/links.h"
 
@@ -30,20 +27,19 @@ GmailModule gmail(&oauth, &gmailToken, "YOUR_EMAIL@gmail.com");
 SteamModule steam(&oauth, &steamToken, "STEAM_ID");
 
 void onPacketReceived(const OpenRF_Packet &pkt) {
-    if (pkt.src != 0x00000001) lora.send(pkt);
-    if (pkt.src != 0x11111111) meshtastic.send(pkt);
-    if (pkt.src != 0x22222222) reticulum.send(pkt);
-    if (pkt.src != 0xDDDDDDDD) discord.send(pkt);
-    if (pkt.src != 0xABCDEF01) sms.send(pkt);
-    if (pkt.src != 0xBBBBBBBB) slack.send(pkt);
-    if (pkt.src != 0xCCCCCCCC) gmail.send(pkt);
-    if (pkt.src != 0xEEEEEEEE) steam.send(pkt);
+    lora.send(pkt);
+    meshtastic.send(pkt);
+    reticulum.send(pkt);
+    discord.send(pkt);
+    sms.send(pkt);
+    slack.send(pkt);
+    gmail.send(pkt);
+    steam.send(pkt);
 }
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
-
     WiFi.begin("YOUR_SSID", "YOUR_PASSWORD");
     while (WiFi.status() != WL_CONNECTED) { delay(500); Serial.print("."); }
     Serial.println("\nWiFi connected!");
@@ -72,12 +68,5 @@ void setup() {
 
 void loop() {
     router.loop();
-
-    if (!oauth.isTokenValid(discordToken)) oauth.refreshAccessToken("CLIENT_ID", "CLIENT_SECRET", discordToken);
-    if (!oauth.isTokenValid(smsToken)) oauth.refreshAccessToken("CLIENT_ID", "CLIENT_SECRET", smsToken);
-    if (!oauth.isTokenValid(slackToken)) oauth.refreshAccessToken("CLIENT_ID", "CLIENT_SECRET", slackToken);
-    if (!oauth.isTokenValid(gmailToken)) oauth.refreshAccessToken("CLIENT_ID", "CLIENT_SECRET", gmailToken);
-    if (!oauth.isTokenValid(steamToken)) oauth.refreshAccessToken("CLIENT_ID", "CLIENT_SECRET", steamToken);
-
     delay(10);
 }
