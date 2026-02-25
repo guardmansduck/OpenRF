362d5e1acad19d1019d26a60e31f0f19ae86af30
guardmansduck <100035465@mvla.net>
2026-02-25 13:30:31 -0800

    Refactor OAuth token refresh logic and update messages

--- main.cpp
+++ main.cpp
@@ -2,21 +2,16 @@
 #include "core/router.h"
 #include "core/packet.h"
 
-// Modules
 #include "modules/lora/lora.h"
 #include "modules/discord/discord.h"
 #include "modules/sms/sms.h"
 
-// OAuth
 #include "oauth/oauth.h"
 #include "config/links.h"
 
-// ======== Global Objects ========
 Router router;
 
 LoRaModule lora;
-
-// Central OAuth manager and tokens
 OAuthManager oauth;
 OAuthToken discordToken;
 OAuthToken smsToken;
@@ -24,20 +19,16 @@ OAuthToken smsToken;
 DiscordModule discord(&oauth, &discordToken, "YOUR_DISCORD_CHANNEL_ID");
 SMSModule sms(&oauth, &smsToken, "+15551234567");
 
-// ======== OpenRF Packet Routing ========
-// This example forwards packets between all modules
 void onPacketReceived(const OpenRF_Packet &pkt) {
-    // Forward packet to all modules except the source
     if (pkt.src != 0x00000001) lora.send(pkt);
     if (pkt.src != 0xDDDDDDDD) discord.send(pkt);
-    if (pkt.src != 0xFFFFFFFF) sms.send(pkt);
+    if (pkt.src != 0xABCDEF01) sms.send(pkt);
 }
 
 void setup() {
     Serial.begin(115200);
     delay(1000);
 
-    // WiFi required for Discord and SMS
     WiFi.begin("YOUR_SSID", "YOUR_PASSWORD");
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
@@ -45,32 +36,26 @@ void setup() {
     }
     Serial.println("\nWiFi connected!");
 
-    // Setup router
     router.addTransport(&lora);
     router.addTransport(&discord);
     router.addTransport(&sms);
 
-    // Set callback for all modules
     lora.setReceiveCallback(onPacketReceived);
     discord.setReceiveCallback(onPacketReceived);
     sms.setReceiveCallback(onPacketReceived);
 
-    // Begin modules
     router.begin();
 
-    Serial.println("OpenRF router is running!");
+    Serial.println("OpenRF router running!");
 }
 
 void loop() {
     router.loop();
 
-    // Optional: refresh tokens periodically
-    if (!oauth.isTokenValid(discordToken)) {
-        oauth.refreshAccessToken("YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET", discordToken);
-    }
-    if (!oauth.isTokenValid(smsToken)) {
-        oauth.refreshAccessToken("YOUR_CLIENT_ID", "YOUR_CLIENT_SECRET", smsToken);
-    }
+    if (!oauth.isTokenValid(discordToken))
+        oauth.refreshAccessToken("CLIENT_ID", "CLIENT_SECRET", discordToken);
+    if (!oauth.isTokenValid(smsToken))
+        oauth.refreshAccessToken("CLIENT_ID", "CLIENT_SECRET", smsToken);
 
-    delay(10); // allow other tasks to run
+    delay(10);
 }
